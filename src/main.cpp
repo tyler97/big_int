f44b4fadbd9a6ef8d5d1a93fe50feef2f52d9db7
Tyler Ruiz <ruiztyler97@gmail.com>
2020-09-30 13:30:11 -0500

    changing folder structure

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main()
     big_int cur(int64_t(1));
     big_int fib(0);
 
-    for (int64_t i = 1; i < int64_t(300); ++i)
+    for (int64_t i = 1; i < int64_t(3000); ++i)
     {
         fib = cur + prev;
         prev = cur;
